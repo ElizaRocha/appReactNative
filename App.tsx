91e676afa6213531ba7a7762acdbc2336e0484f4
Eliza Rocha <eliza.rocha@codificar.com.br>
2022-06-03 13:08:46 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SafeAreaView, Text, StyleSheet} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <Text style={styles.text}>Hello World</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 15,
+    marginVertical: 10,
+    marginHorizontal: 10,
+    fontFamily: 'Montserrat-Regular'
+  },
+});
+
+export default App;
